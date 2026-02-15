9240c659ddab704ab845c47ed303a82d9d2e6b20
Henrique Ribeiro <hr7.xit@gmail.com>
2026-02-15 17:49:56 -0300

    api Rest

---
+++ api-express/src/server.ts
@@ -0,0 +1,8 @@
+console.log("Hello World");
+
+function soma(a: number, b: number):number{
+    return a + b
+}
+const resultado:number = soma(1,2)
+console.log(`o resultado da soma é: ${resultado}`);
+
